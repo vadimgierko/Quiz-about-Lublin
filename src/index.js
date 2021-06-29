a3232b7d62fd0bd48cdd9f0989227490ea1b8ffe
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-06-29 17:22:10 +0200

    Styling and architecture done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Quiz about Lublin will be here :-)</h1>
+      <Container
+        style={{textAlign: "center"}}
+        fluid 
+      >
+        <Jumbotron>
+          <h1>Quiz o Lublinie</h1>
+          <h2>Sprawdź swoją wiedzę na temat Lublina!</h2>
+          <p>Możesz najpierw przeczytać <a target="_blank" rel="noreferrer" href="https://pl.wikipedia.org/wiki/Lublin">artykuł o Lublinie na Wikipedii</a>, z którego zaczerpnąłem pytania.</p>
+          <p>Albo... od razu przejdź do rozwiązywania quizu poniżej!</p>
+          <Button variant="primary">Zacznij quiz!</Button>
+        </Jumbotron>
+        <Form>
+          <Form.Group>
+            <Form.Label>Przykładowe pytanie</Form.Label>
+            <Form.Control style={{textAlign: "center"}} as="select" multiple>
+              <option>Przykładowa odpowiedź 1</option>
+              <option>Przykładowa odpowiedź 2</option>
+              <option>Przykładowa odpowiedź 3</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <footer>Designed and programmed by Vadim Gierko 2021</footer>
+      </Container>
     );
   }
 }
